fc443bb1d3157d88457eb7a3ae54fc559d332744
Jia Meng Leong <jiamengleong@Jias-MacBook-Pro.local>
2019-07-31 03:35:36 +0800

    Migration

--- db/migrate/20190730051256_create_exam_sessions.rb
+++ db/migrate/20190730051256_create_exam_sessions.rb
@@ -1,7 +1,9 @@
 class CreateExamSessions < ActiveRecord::Migration[5.2]
   def change
     create_table :exam_sessions do |t|
-
+      t.string :name
+      t.datetime :from
+      t.datetime :until
       t.timestamps
     end
   end
